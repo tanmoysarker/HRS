b8fe62074c6988f8d6c51690ca849834bf7f0b96
Tanmoy Sarker <25175610+tanmoysarker@users.noreply.github.com>
2024-11-06 10:11:20 +1100

    Delete EMRSimulationWebApp directory

--- EMRSimulationWebApp/EMRSimulationWebApp/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace EMRSimulationWebApp.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
